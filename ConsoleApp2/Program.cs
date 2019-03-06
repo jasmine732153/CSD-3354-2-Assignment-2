5376b2097b85282b268557fe34838121a530b88d
visualstudio <visualstudio@122-PC33>
2019-03-06 11:00:09 -0500

    final commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace  DelegateAndEvents
-
+namespace DelegatesAndEvents
 {
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method2(int i)
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method2);
+            myDelegate();
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
         }
     }
+
 }
