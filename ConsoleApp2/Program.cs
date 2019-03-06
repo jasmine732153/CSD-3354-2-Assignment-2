7062077c648dd6c1cd0ebec87e9eca0178eba1c0
visualstudio <visualstudio@122-PC33>
2019-03-06 11:22:55 -0500

    initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,50 +9,8 @@ using System.Threading.Tasks;
 //Assignment 2
 //march6,19
 
-
 using System;
 
-    namespace DelegatesAndEvents
-    {
-        public class Program
-        {
-            public static void Main()
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-        }
-
-        public class DelegateExercises
-        {
-            public delegate int MyDelegate(int intValue);
-
-            int Method1(int intMethod1)
-            {
-                return intMethod1 * 4;
-            }
-
-            int Method2(int intMethod1)
-            {
-                return intMethod1 * 20;
-            }
-
-            public void Method4(MyDelegate myDelegate)
-            {
-                for (int i = 1; i <= 5; i++)
-                    System.Console.Write(myDelegate(i) + " ");
-            }
-
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                Method4(myDelegate);
-                myDelegate = new MyDelegate(Method2);
-                Method4(myDelegate);
-            }
-        }
-    }
-
-
-
+namespace DelegatesAndEvents
+{
+    
