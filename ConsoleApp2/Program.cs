9b22334def7649f7157ed867e335a4160be3ea1b
visualstudio <visualstudio@122-PC33>
2019-03-06 11:31:19 -0500

    final commit 72660 732153

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,4 +13,38 @@ using System;
 
 namespace DelegatesAndEvents
 {
-   
+    public class Program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            try
+            {
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            throw new System.Exception();
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+}
