a64bdc75144f2ca783bccdd0ac90039e362e11fa
visualstudio <visualstudio@122-PC33>
2019-03-06 10:50:53 -0500

    initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,19 +14,3 @@ namespace  DelegateAndEvents
         }
     }
 }
-public class  DelegateExercises
-{
-    public delegate void MyDelegate();
-
-    void Method1()
-    {
-        Console.WriteLine("Method1");
-        Console.ReadLine();
-    }
-    public void Method2()
-    {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate();
-    }
-}
-
